b46090080ecd3cf112cc706772b5585c594f2329
luispoz <luispoz@gmail.com>
2021-10-13 11:02:43 -0600

    add html template

---
+++ public/js/app.js
@@ -0,0 +1,16 @@
+
+const requestModal = document.querySelector('.new-request');
+const requestLink = document.querySelector('.add-request');
+
+
+// open request modal
+requestLink.addEventListener('click', () => {
+    requestModal.classList.add('open');
+});
+
+// close request modal
+requestModal.addEventListener('click', (e) => {
+    if (e.target.classList('new-request')) {
+        requestModal.classList('open');
+    }
+});
